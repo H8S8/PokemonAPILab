06a1412c62ac7a7114eff276dd1db65adabf1604
Sahil Patel <sahilpatel1906@gmail.com>
2024-03-27 09:10:36 -0500

    fetched Pokemon data

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import PokemonList from "../components/PokemonList";
 
 const PokemonContainer = () => {
+
+    const [pokemon, setPokemon] = useState([]);
+    
+    const loadData = async () => {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        const jsonData = await response.json();
+        setPokemon(jsonData.results);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, [])
+
     return (
+        // https://pokeapi.co/api/v2/pokemon/
         <>
         <p> Pokemon Container </p>
         <PokemonList />
